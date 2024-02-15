836cce91d329e4ec33b159f7a56b0e3f9366ef64
Ayoub-UA <117929082+Ayoub-UA@users.noreply.github.com>
2024-02-15 12:11:08 +0100

    added loop method

--- src/be/uantwerpen/fti/ei/HelloWorld.java
+++ src/be/uantwerpen/fti/ei/HelloWorld.java
@@ -6,4 +6,15 @@ public class HelloWorld {
     {
     System.out.println("HelloWorld");
     }
+
+    public void looplHello(int amount)
+    {
+        int i=0;
+        if(i<amount)
+        {
+            i=i+1;
+            print();
+        }
+    }
+
 }
